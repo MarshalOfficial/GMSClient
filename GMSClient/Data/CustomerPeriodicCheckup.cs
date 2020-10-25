09fdecbac1cdc597a79bb7a738933a6d4a881903
MohadmadReza <mr.rahmatzade371@gmail.com>
2020-10-25 13:46:29 +0330

    feat add customer profile

---
+++ GMSClient/Data/CustomerPeriodicCheckup.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GMSClient.Data
+{
+    public class CustomerPeriodicCheckup
+    {
+        public long ID { get; set; }
+
+        public long AccountID { get; set; }
+
+        public DateTime? RegDate { get; set; }
+
+        public double? Height { get; set; }
+
+        [Required]
+        public double? Weight { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public double? WaistSize { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public double? Chest { get; set; }
+
+        public double? ABS { get; set; }
+
+        public double? BUTT { get; set; }
+
+        public double? RightArm { get; set; }
+
+        public double? LeftArm { get; set; }
+
+        public double? RightThigh { get; set; }
+
+        public double? LeftThigh { get; set; }
+
+        public double? RightCalves { get; set; }
+
+        public double? LeftCalves { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public string CreateDateFa { get; set; }
+
+        [Required]
+        public string RegDateFa { get; set; }
+
+        public string FullName { get; set; }
+
+    }
+}
--- GMSClient/Extension/Extensions.cs
+++ GMSClient/Extension/Extensions.cs
@@ -1,20 +1,30 @@
 ﻿using GMSClient.Data;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GMSClient.Extension
 {
     public static class Extensions
     {
+        
         public static string GetBackendErrorMsg(this string json)
         {
-            var data = JsonConvert.DeserializeObject<ErrorEntity[]>(json);
-            if (data != null && data[0] != null && data[0].Result.ToStringEmpty().ToLower().Contains("error") && !string.IsNullOrWhiteSpace(data[0].Txt))
+            try
             {
-                return data[0].Txt;
+                var data = JsonConvert.DeserializeObject<ErrorEntity[]>(json);
+                if (data != null && data[0] != null && data[0].Result.ToStringEmpty().ToLower().Contains("error") && !string.IsNullOrWhiteSpace(data[0].Txt))
+                {
+                    return data[0].Txt;
+                }
+                else
+                    return string.Empty;
             }
-            else
+            catch
+            {
                 return string.Empty;
+            }
         }
 
         public static string ToStringEmpty(this object str)
