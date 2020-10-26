587d43b402020228ee72d3cdbcb8599b3f116c8a
MohadmadReza <mr.rahmatzade371@gmail.com>
2020-10-26 09:27:23 +0330

    feat add shiftpatterns page

---
+++ GMSClient/Data/ShiftPatternDetails.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GMSClient.Data
+{
+    public class ShiftPatternDetails
+    {
+        public long ID { get; set; }
+
+        public long ShiftPatternHeaderID { get; set; }
+
+        public TimeSpan FromTime { get; set; }
+
+        public TimeSpan ToTime { get; set; }
+
+        public bool Gender { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public string CreateDateFa { get; set; }
+    }
+}
---
+++ GMSClient/Data/ShiftPatternHeader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GMSClient.Data
+{
+    public class ShiftPatternHeader
+    {
+        public long ID { get; set; }
+        
+        public string Title { get; set; }
+        
+        public bool IsDeleted { get; set; }
+        
+        public DateTime CreateDate { get; set; }
+
+        public string CreateDateFa { get; set; }
+    }
+}
