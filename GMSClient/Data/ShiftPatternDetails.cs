ada42102df2721ca2a2eb9fae2daec164728861f
MohadmadReza <mr.rahmatzade371@gmail.com>
2020-10-29 01:35:52 +0330

    feat add shift pattern config page

--- GMSClient/Data/ShiftPatternDetails.cs
+++ GMSClient/Data/ShiftPatternDetails.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GMSClient.Data
 {
@@ -12,10 +13,21 @@ namespace GMSClient.Data
 
         public TimeSpan ToTime { get; set; }
 
+        [Required]
+        public DateTime? FromTimeF { get; set; }
+
+        [Required]
+        public DateTime? ToTimeF { get; set; }
+
+
         public bool Gender { get; set; }
 
         public DateTime CreateDate { get; set; }
 
         public string CreateDateFa { get; set; }
+
+        public string GenderFa { get { return Gender ? "مرد" : "زن"; } }
+
+        public string HeaderTitle { get; set; } 
     }
 }
