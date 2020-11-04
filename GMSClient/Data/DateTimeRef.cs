718f59976f973d42631d1668d642a133276dab9f
MohadmadReza <mr.rahmatzade371@gmail.com>
2020-11-05 01:02:42 +0330

    feat add calendar config

---
+++ GMSClient/Data/DateTimeRef.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GMSClient.Data
+{
+    public class DateTimeRef
+    {
+        public long DateTimeRef_ID { get; set; }
+        public string DaySh { get; set; }
+        public string WeekDayShName { get; set; }
+        public string DateSh { get; set; }
+
+        public string MonthShName { get; set; }
+
+        public long ShiftPatterHeaderID { get; set; }
+
+        public string ShiftPatterHeaderTitle { get; set; }    
+
+    }
+}
+    
